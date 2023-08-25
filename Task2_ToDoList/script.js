7653d3f5bf27460ceecceda2b4695b2a8d8a4705
Shivam Srivastava <126308258+211B291@users.noreply.github.com>
2023-08-25 23:45:33 +0530

    Add files via upload

--- Task2_ToDoList/script.js
+++ Task2_ToDoList/script.js
@@ -1,133 +1,42 @@
-html {
-    height: 100%;
-    width: 100%;
+let inputBox = document.getElementById("form2");
+// console.log(inputBox)
+let listContainer = document.getElementById("list-group")
+// console.log(listContainer)
+let btn = document.getElementById("bu")
+// console.log(bu)
+btn.addEventListener('click',function(e){
+  if(inputBox.value === ''){
+    e.preventDefault()
+    alert("Task Bar Empty")
   }
-  
-  .gradient-custom {
-    min-height: 100vh;
-    background: radial-gradient(50% 123.47% at 50% 50%, #00ff94 0%, #720059 100%),
-      linear-gradient(121.28deg, #669600 0%, #ff0000 100%),
-      linear-gradient(360deg, #0029ff 0%, #8fff00 100%),
-      radial-gradient(100% 164.72% at 100% 100%, #6100ff 0%, #00ff57 100%),
-      radial-gradient(100% 148.07% at 0% 0%, #fff500 0%, #51d500 100%);
-    background-blend-mode: screen, color-dodge, overlay, difference, normal;
+  else{
+    e.preventDefault()
+    let li = document.createElement("li")
+    li.innerHTML = inputBox.value
+    listContainer.appendChild(li)
+    let span = document.createElement("span")
+    span.innerHTML = "\u00D7"
+    li.appendChild(span)
   }
-  .form-outline{
-    flex-direction: column;
-    padding: 0.5rem;
-    margin: 1rem auto;
+  inputBox.value = '';
+  local()
+})
+
+listContainer.addEventListener('click',function(e){
+  if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked")
+    local()
   }
-  
-  .btn-info {
-    transform: translateY(-40%);
-    padding: 0.2rem;
-    margin: 0.5rem;
+  else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove()
+    local()
   }
-  .card{
-    /* border: 10px solid blue; */
-    margin-top: 5rem;
-    box-shadow: 5px 5px 10px #484242;
-    transform: translateY(300%);
-    animation: card 1s forwards 0.5s ease-in-out;
-  }
-  @keyframes card{
-    100%{
-      width: 100%;
-      transform: translateY(0%);
-    }
-  }
-  .card-body{
-    padding: auto;
-    margin: 0.2 rem auto;
-  }
-  form{
-    flex-wrap: wrap;
-  }
-  
-  .todo-app {
-    color: yellow;
-    text-align: center;
-    border: 3px solid black;
-    background-color: #555;
-    border-radius: 10px;
-    box-shadow: 2px 2px 5px #484242;
-    margin-bottom: 1rem;
-    transform: translateY(-110%);
-    animation: todo 1s forwards 0.5s ease-in-out;
-    width: 30%;
-  }
-  @keyframes todo{
-    100%{
-      width: 100%;
-      transform: translateY(80%);
-    }
-  }
-  
-  ul{
-    display: flex;
-  }
-  ul li{
-    list-style: none;
-    /* text-align: center; */
-    font-size: 17px;
-    padding: 12px 8px 12px 50px;
-    user-select: none;
-    cursor: pointer;
-    position: relative;
-    background: lightskyblue;
-    border-radius: 200px;
-    font-weight: 400;
-    font-family: sans-serif;
-    box-shadow: 4px 4px 8px #2f2d2d;
-    margin:0.5rem 0;
-  }
-  .list-group li::before{
-    
-    content:'';
-    position: absolute;
-    height: 25px;
-    width: 25px;
-    border-radius: 50%;
-    background-image: url(unchecked.png);
-    background-size: cover;
-    background-position: center;
-    top: 12px;
-    left:8px;
-    /* margin-right: 5rem; */
-  }
-  .list-group .checked{
-    color: red;
-    text-decoration: line-through;
-    transition: text-decoration 0.3s ease-in-out;
-  }
-  .list-group .checked::before{
-    content:'';
-    position: absolute;
-    height: 25px;
-    width: 25px;
-    border-radius: 50%;
-    background-image: url(download.jpg);
-    background-size: cover;
-    background-position: center;
-    top: 12px;
-    left:8px;
-  }
-  ul li span{
-    position: absolute;
-    right:5px;
-    top: 5px;
-    width: 40px;
-    height: 40px;
-    font-size: 28px;
-    color: #555;
-    line-height: 40px;
-    text-align: center;
-    border-radius: 50%;
-    background: lightblue;
-    /* border: 1px solid blue; */
-  }
-  ul li span:hover{
-    color: #fff;
-    background: red;
-  }
-  
+}, false)
+
+function local(){
+  localStorage.setItem("data",listContainer.innerHTML)
+}
+function display(){
+  listContainer.innerHTML = localStorage.getItem("data")
+}
+display()
